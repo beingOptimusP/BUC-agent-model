817e8b45b9a4e1217adf42cdc56f738cb8b73fcb
vamsi <75676297+beingOptimusP@users.noreply.github.com>
2021-07-14 01:10:57 +0530

    added libraries

--- test/test.js
+++ test/test.js
@@ -5,13 +5,10 @@ contract("Test for deployement", async accounts => {
         const instance = await BUC.deployed();
         const balance = await instance.balanceOf.call(accounts[0]);
         assert.equal(balance.valueOf() / Math.pow(10, 18), 1000000000);
+    })
+    it("checking total supply", async accounts => {
+        const instance = await BUC.deployed();
+        const supply = await instance.totalSupply.call(accounts[0]);
+        assert.equal(supply / Math.pow(10, 18), 1000000000)
     });
-
-    // it("checking total supply", async accounts => {
-    //     const instance = await BUC.deployed();
-    //     const supply = await instance.totalSupply.call();
-    //     assert.equal(supply / Math.pow(10, 18), 1000000000)
-    // });
-
-});  
-    
+});
